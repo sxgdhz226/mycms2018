e37faf8179f84222f20ceef7f238a0d839df38de
sxgdhz226 <1600328315@qq.com>
2018-09-28 11:12:51 +0800

    加入activiti activemq

---
+++ src/main/java/com/ruoyi/framework/config/properties/PropertiesConfig.java
@@ -0,0 +1,11 @@
+package com.ruoyi.framework.config.properties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = {"classpath:/properties/activemq.properties"})  //多个属性文件以逗号隔开
+//@ImportResource({"classpath:/application/spring-*.xml"})
+public class PropertiesConfig {
+}
